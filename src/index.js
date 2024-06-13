a3ef02e08df24b2feabbebac9350d3deac4ba10e
Alexferraz2 <alex_ferraz2@hotmail.com>
2024-06-12 22:03:32 -0300

    Configurado webpack do projeto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World 2')
+  res.send('Hello World, agora vai dar certo')
 })
 
 app.listen(3000, () => console.log('OnLine: http://localhost:3000'))
